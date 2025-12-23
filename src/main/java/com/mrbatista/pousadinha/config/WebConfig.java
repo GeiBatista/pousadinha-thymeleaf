40262193a30e49e09df20959c2c7feeab33af7b5
geibatista <geibatista@hotmail.com>
2025-12-22 23:25:09 -0300

    Configurando viewResolver com o thymeleaf

--- src/main/java/com/mrbatista/pousadinha/config/WebConfig.java
+++ src/main/java/com/mrbatista/pousadinha/config/WebConfig.java
@@ -1,9 +1,20 @@
 package com.mrbatista.pousadinha.config;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring6.ISpringTemplateEngine;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 import com.mrbatista.pousadinha.controller.HospedeController;
 
@@ -11,5 +22,48 @@ import com.mrbatista.pousadinha.controller.HospedeController;
 @ComponentScan(basePackageClasses = { HospedeController.class })
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+	
+	private ApplicationContext applicationContext;
+
+//    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+//        this.applicationContext = applicationContext;
+//    }
+    
+    public WebConfig(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+    
+    @Bean
+    public ViewResolver viewResolver() {
+    	ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+    	resolver.setTemplateEngine(templateEngine());
+    	resolver.setCharacterEncoding("UTF-8");
+    	return resolver;
+    }
+    
+    @Bean
+    public ISpringTemplateEngine templateEngine()
+    {
+    	SpringTemplateEngine engine = new SpringTemplateEngine();
+    	engine.setEnableSpringELCompiler(true);
+    	engine.setTemplateResolver(templateResolver());
+    	return engine;
+    }
+    
+    private ITemplateResolver templateResolver() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setApplicationContext(applicationContext);
+        resolver.setPrefix("classpath:/templates/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode(TemplateMode.HTML);
+        return resolver;
+    }
+
+@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    	registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+    	
+    }
+
 
 }
--- src/main/java/com/mrbatista/pousadinha/controller/HospedeController.java
+++ src/main/java/com/mrbatista/pousadinha/controller/HospedeController.java
@@ -1,8 +1,13 @@
 package com.mrbatista.pousadinha.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HospedeController {
-
+	
+	@RequestMapping("/hospedes")
+	public String novo() {
+		return "hospede/CadastroHospede";
+	}
 }
