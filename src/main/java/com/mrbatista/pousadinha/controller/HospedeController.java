fd4a523be8b4147f34ccb1991fe060b6305184be
geibatista <geibatista@hotmail.com>
2025-12-23 22:57:27 -0300

    iniciando formulario de cadastro de hóspede

--- src/main/java/com/mrbatista/pousadinha/controller/HospedeController.java
+++ src/main/java/com/mrbatista/pousadinha/controller/HospedeController.java
@@ -1,13 +1,22 @@
 package com.mrbatista.pousadinha.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mrbatista.pousadinha.model.Hospede;
 
 @Controller
 public class HospedeController {
 	
-	@RequestMapping("/hospedes")
+	@RequestMapping("/hospedes/novo")
 	public String novo() {
+		return "hospede/CadastroHospede";
+	}
+	
+	@PostMapping("/hospedes/novo")
+	public String salvar(Hospede hospede) {
+		System.out.println("Salvando hospede... " + hospede.getNome() + " " + hospede.getSobrenome());
 		return "hospede/CadastroHospede";
 	}
 }
